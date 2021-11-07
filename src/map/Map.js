7f49b38b40357da7e87c05a6112f43319335e70d
Ilya Bakunovskiy <ilya@Ilyas-MacBook-Pro.local>
2021-11-07 22:15:40 +0100

    save commit

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { Source, Layer } from 'react-map-gl'
+import mapGrid from './consts/map-grid.json'
 
 function Map() {
   const [viewport, setViewport] = useState({
@@ -13,28 +14,50 @@ function Map() {
   // const [isLoding, setIsLoaging] = useState(false)
   // const [isError, setIsError] = useState(false)
 
-  // useEffect(() => {
-  //   setIsLoaging(true)
-  //   fetch('api')
-  //     .then(res => {
-  //       if (res.status === 200) {
-  //         setData(res.body.data)
-  //       }
-  //     })
-  //     .catch(error => {
-  //       setIsError(true)
-  //     })
+  useEffect(() => {
+    // setIsLoaging(true)
+    const date = new Date()
+    const today = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear()
+    console.log(mapGrid.features)
+    const points = mapGrid.features.map(item => (
+      {
+        ...item,
+        properties: {
+          center: [
+            item.geometry.coordinates[0][0][0] + ((item.geometry.coordinates[0][2][0] - item.geometry.coordinates[0][0][0]) / 2),
+            item.geometry.coordinates[0][0][1] + ((item.geometry.coordinates[0][2][1] - item.geometry.coordinates[0][0][1]) / 2)
+          ],
+        }
+      }
+    ))
 
-  //     return () => clearInterval()
-  // }, [])
+    for (let i = 0; i < 3; i++) {
+      fetch(`http://localhost:8000/weatherByLongLatAndDate?lat=${points[i].properties.center[0]}&lon=${points[i].properties.center[1]}&date=${today}`)
+        .then(res => {
+          if (res.status === 200) {
+            console.log(res.json())
+          }
+        })
+        .catch(error => {
+          console.error(error)
+          // setIsError(true)
+        })
+    }
+
+    // return () => clearInterval()
+  }, [])
 
   return (
     <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken='pk.eyJ1IjoiYXZlbHVkb3dpayIsImEiOiJja3ZvODAxdXYwcnZsMnBqcGFsdGFvYnpsIn0.GXnlJJJPUArw7jszNQJ0eQ'
-        onViewportChange={(viewport) => setViewport(viewport)}
-        mapStyle='mapbox://styles/mapbox/light-v9'
-      />
+      {...viewport}
+      mapboxApiAccessToken='pk.eyJ1IjoiYXZlbHVkb3dpayIsImEiOiJja3ZvODAxdXYwcnZsMnBqcGFsdGFvYnpsIn0.GXnlJJJPUArw7jszNQJ0eQ'
+      onViewportChange={(viewport) => setViewport(viewport)}
+      mapStyle='mapbox://styles/mapbox/light-v9'
+    >
+      {/* <Source type='geojson' data={data}>
+        <Layer {...dataLayer} />
+      </Source> */}
+    </ReactMapGL>
   )
 }
 export default Map
